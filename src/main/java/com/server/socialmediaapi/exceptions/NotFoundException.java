ea4656498bbfcbad9728283a9f8443de710144e2
Konstantin Nikolaenko <nikolaenko.kostya.vuz@mail.ru>
2023-06-19 01:59:51 +0300

    Create exceptions and error response

---
+++ src/main/java/com/server/socialmediaapi/exceptions/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.server.socialmediaapi.exceptions;
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
